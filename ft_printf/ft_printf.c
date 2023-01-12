643bac3f1d30b55cd6c8c1b6f5919c795796ae63
Sergio Garcia <sergio@atratopago.com>
2023-01-12 08:41:26 +0100

    ckpt

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: segarcia <segarcia@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 04:28:47 by segarcia          #+#    #+#             */
-/*   Updated: 2023/01/11 06:42:01 by segarcia         ###   ########.fr       */
+/*   Updated: 2023/01/12 08:22:10 by segarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-int	ft_printf_char(int c)
+int ft_printf_char(int c)
 {
 	write(1, &c, 1);
 	return (1);
@@ -35,12 +35,12 @@ int ft_printf_str(char *str)
 		ft_printf_char(str[i]);
 		i++;
 	}
-	return (i);
+	return (i);	
 }
 
-void ft_printf_num(long long int num, int base, int *len)
+void ft_print_num(long long int num, int base, int *len)
 {
-	char	*hexadecimal = "0123456789abcdef";
+	char *hexadecimal = "0123456789abcdef";
 
 	if (num < 0)
 	{
@@ -48,68 +48,57 @@ void ft_printf_num(long long int num, int base, int *len)
 		*len += write(1, "-", 1);
 	}
 	if (num >= base)
-		ft_printf_num((num / base), base, len);
-	*len += write(1, &hexadecimal[num % base], 1);
-}
-
-void ft_printf_hexA(long long int num, int base, int *len)
-{
-	char	*hexadecimal = "0123456789ABCDEF";
-
-	if (num < 0)
-	{
-		num *= -1;
-		*len += write(1, "-", 1);
-	}
-	if (num >= base)
-		ft_printf_num((num / base), base, len);
+		ft_print_num((num / base), base, len);
 	*len += write(1, &hexadecimal[num % base], 1);
 }
 
 int ft_printf(const char *str, ...)
 {
-	int 	i;
-	int 	print_len;
-	va_list	arg;
+	int			i;
+	int			print_len;
+	va_list		arg;
 
 	i = 0;
 	print_len = 0;
 	va_start(arg, str);
-	
 	while (str[i])
 	{
 		if (str[i] == '%')
 		{
-			if (str[i + 1] == 'c')
-				print_len += ft_printf_char(va_arg(arg, int));
-			if (str[i + 1] == 's')
-				print_len += ft_printf_str(va_arg(arg, char *));
-			if (str[i + 1] == 'i' || str[i + 1] == 'd' )
-				ft_printf_num((long long int)va_arg(arg, int), 10, &print_len);
-			if (str[i + 1] == 'x')
-				ft_printf_num((long long int)va_arg(arg, int), 16, &print_len);
-			if (str[i + 1] == 'X')
-				ft_printf_hexA((long long int)va_arg(arg, int), 16, &print_len);
-			if (str[i + 1] == '%')
-				print_len += ft_printf_char('%');
-			i++;
+			if (str[i + 1])
+			{
+				if (str[i + 1] == 'c')
+					print_len += ft_printf_char(va_arg(arg, int));
+				if (str[i + 1] == 's')
+					print_len += ft_printf_str(va_arg(arg, char *));
+				if (str[i + 1] == 'd' || str[i + 1] == 'i')
+					ft_print_num(va_arg(arg, int), 10, &print_len);
+				if (str[i + 1] == 'x')
+					ft_print_num(va_arg(arg, unsigned int), 16, &print_len);
+				if (str[i + 1] == '%')
+					print_len += ft_printf_char('%');
+				i++;
+			}
 		}
 		else
 			print_len += ft_printf_char(str[i]);
 		i++;
 	}
 	va_end(arg);
-	return(print_len);
+	return (print_len);
 }
 
-int main()
-{
-	ft_printf(" - Len: %i\n", ft_printf("%c", 'a'));
-	ft_printf(" - Len: %i\n", ft_printf("%s", "toto"));
-	ft_printf(" - Len: %i\n",ft_printf("%%"));
-	ft_printf(" - Len: %i\n", ft_printf("Magic %s is %d", "number", 42));
-	ft_printf(" - Len: %i\n", ft_printf("Hexadecimal for %d is %x", 42, 42));
-	ft_printf(" - Len: %i\n", ft_printf("Hexadecimal for %d is %X\n", 42, 42));
-	ft_printf(" - Len: %i\n", printf("Hexadecimal for %d is %X\n", 42, 42));
-	return (1);
-}
+// int main()
+// {
+// 	int len;
+// 	len = ft_printf("w%w%w%w");
+// 	printf("\nlen: %i\n", len);
+// 	// ft_printf(" - Len: %i\n", ft_printf("%c", 'a'));
+// 	// ft_printf(" - Len: %i\n", ft_printf("%s", "toto"));
+// 	// ft_printf(" - Len: %i\n",ft_printf("%%"));
+// 	// ft_printf(" - Len: %i\n", ft_printf("Magic %s is %d", "number", 42));
+// 	// ft_printf(" - Len: %i\n", ft_printf("Hexadecimal for %d is %x", 42, 42));
+// 	// ft_printf(" - Len: %i\n", ft_printf("Hexadecimal for %d is %X\n", 42, 42));
+// 	// ft_printf(" - Len: %i\n", printf("Hexadecimal for %d is %X\n", 42, 42));
+// 	return (1);
+// }
